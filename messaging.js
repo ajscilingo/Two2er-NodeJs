daea72ecb7eeddfa2d3a0fb9efaec85cb8e4d6d4
Anthony Scilingo <ajscilingo@gmail.com>
2017-03-12 12:43:40 -0500

    Endpoint/Route integration with Firebase Messaging
    The booking/reject booking/accept and booking/request routes now send Firebase notification messages to users if they have at least one fcm_token in their fcm_tokens user attribute.  Coordination with the Android side with regard to adding and removing fcm_tokens will be neccessary.
    
      Changes to be committed:
    	modified:   messaging.js
    	modified:   package.json
    	modified:   routes/booking.js

--- messaging.js
+++ messaging.js
@@ -1,10 +1,7 @@
-const gcm = require('node-gcm');
+const FCM = require('fcm-push');
 
 // Set up Sender with Google Cloud Messaging API Key
-const sender = new gcm.Sender('AAAAmDX3iUY:APA91bHOtkey6pXpiURB1mvWKOf-Dgg5lcbuR4LFyVTqSwa2VOAciMH7_Xu8MQQUDw_hnH-omMeQpjA23Q3sTCnKuBxXp1r-6JpjhG_rx633ZTaofMjDE0TQjK_ZuyT0DbUFVGwBhK_X');
+var api_key = 'AAAAmDX3iUY:APA91bHOtkey6pXpiURB1mvWKOf-Dgg5lcbuR4LFyVTqSwa2VOAciMH7_Xu8MQQUDw_hnH-omMeQpjA23Q3sTCnKuBxXp1r-6JpjhG_rx633ZTaofMjDE0TQjK_ZuyT0DbUFVGwBhK_X'
+var fcm = new FCM(api_key);
 
-function sendBookingNotificationToUser(user_id, booking_id, status){
-    
-    // Set up Message to be sent    
-    var message = new gcm.Message();
-}
+module.exports = fcm;
--- routes/booking.js
+++ routes/booking.js
@@ -10,6 +10,101 @@ const Location = require('../models/schemas/location.js')
 const mongoose = require('mongoose');
 const dateFormat = require('dateformat');
 const Promise = require('bluebird');
+// Enums used for BookingStatus
+const BookingStatus = require('../enums/bookingstatus.js');
+const fcm = require('../messaging.js');
+
+/**
+ * Submit booking request and send Firebase Notification to Tutor 
+ * @param {*} err 
+ * @param {*} tutor 
+ * @param {*} student 
+ * @param {*} booking 
+ * @param {*} res 
+ */
+function saveBookingAndSendNotificationToTutor(err, tutor, student, booking, res){
+    // if tutor not found send error
+    if(err){
+        res.status(404).send(err);
+    }
+
+    // update booking to include tutor's name
+    booking.tutor_name = tutor.name;
+    
+    // try to save booking and send notification, if can't send error
+    booking.save( (err) => {
+        if(err) 
+            res.status(404).send(err);
+        
+        // send a message only if there's fcm_tokens
+        if(tutor.fcm_tokens.length > 0){ 
+            
+            // create new notification message
+            var fcm_message = {
+                registration_ids: tutor.fcm_tokens,
+                notification : {
+                    title: 'Booking Requested',
+                    body: `Booking Requested By ${student.name}`
+                }
+            };
+
+            // attempt to send notification message
+            fcm.send(fcm_message, (err, res) => {
+                if(err)
+                    console.log("failure sending message");
+                else
+                    console.log(`Successfully sent with response: ${res}`);
+            });
+        }
+
+        console.log(`${booking.student_user_id} requesting appointment`);
+        res.json({message: `${booking.student_user_id} requesting appointment`});
+
+    });
+}
+
+
+function updateBookingStatusAndSendNotificaitonToStudent(booking_id, status, res){
+    Booking.findByIdAndUpdate(booking_id, {status: status}, (err, booking) => {
+        if(err)
+            res.status(404).send(err);
+
+        User.findById(mongoose.Types.ObjectId(booking.tutor_user_id), (err, tutor) => {
+            if(err){
+                res.status(404).send(err);
+            }
+            
+            User.findById(mongoose.Types.ObjectId(booking.student_user_id), (err, student) => {
+                if(err){
+                    res.status(404).send(err);
+                }
+                
+                // send a message only if there's fcm_tokens
+                if(student.fcm_tokens.length > 0){
+                        // create new message 
+                    var fcm_message = {
+                        registration_ids: student.fcm_tokens,
+                        notification : {
+                            title: `Booking ${status}`,
+                            body: `Booking ${status} By ${tutor.name}`
+                        }
+                    };
+
+                    fcm.send(fcm_message, (err, res) => {
+                        if(err)
+                            console.log("failure sending message");
+                        else
+                            console.log(`Successfully sent with response: ${res}`);
+                    });
+                }
+
+                console.log(`Booking ${booking._id} has been ${status} by ${tutor._id}`);
+                res.json({message: `Booking ${booking._id} has been ${status} by ${tutor._id}`});
+            });
+            
+        });
+    });
+}
 
 // a middleware function with no mount path. This code is executed for every request to the router
 router.use(function (req, res, next) {
@@ -41,24 +136,9 @@ router.post ('/request', (req, res) => {
             if(student){
                 booking.student_name = student.name;
                 User.findById(mongoose.Types.ObjectId(req.body.tutor_user_id), (err, tutor) => {
-                    if(tutor){
-                        booking.tutor_name = tutor.name;
-                        booking.save( (err) => {
-                            if(err) 
-                                res.status(404).send(err);
-                            else{
-                                console.log(`${booking.student_user_id} requesting appointment`);
-                                res.json({message: `${booking.student_user_id} requesting appointment`});
-                            }
-                        });
-                    }
-                    else{
-                        res.status(404).send(err) ;
-                    }
+                    // save booking and send notification to tutor
+                    saveBookingAndSendNotificationToTutor(err, tutor, student, booking ,res);
                 });
-            }
-            else{
-                res.status(404).send(err);
             }
         });
     }
@@ -69,19 +149,13 @@ router.post ('/request', (req, res) => {
 
 // Route for tutor to reject booking
 // Also used later if tutor needs to cancel
+// To use this route you must be a tutor
 router.post('/reject', (req, res) => {
 
      console.log(`${req.ip} is doing a POST via /booking/reject`);
 
      if(req.user){ 
-        Booking.findByIdAndUpdate(req.body.booking_id, {status: "rejected"}, (err, booking) =>{
-            if(err)
-                re.status(404).send(err);
-            else{
-                console.log(`Booking ${booking._id} has been rejected by ${req.user.customData.user_id}`);
-                res.json({message: `Booking ${booking._id} has been rejected by ${req.user.customData.user_id}`});
-            }
-        });
+        updateBookingStatusAndSendNotificaitonToStudent(req.body.booking_id, BookingStatus.rejected.name, res);
      }
      else{
         res.json({tutor_user_id: 0});
@@ -90,19 +164,12 @@ router.post('/reject', (req, res) => {
 });
 
 // Route for tutor to confirm booking
-router.post('/confirm', (req, res) => {
+router.post('/accept', (req, res) => {
 
-    console.log(`${req.ip} is doing a POST via /booking/confirm`)
+    console.log(`${req.ip} is doing a POST via /booking/accept`)
 
     if(req.user){ 
-        Booking.findByIdAndUpdate(req.body.booking_id, {status: "confirmed"}, (err, booking) =>{
-            if(err)
-                re.status(404).send(err);
-            else{
-                console.log(`Booking ${booking._id} has been confirmed by ${req.user.customData.user_id}`);
-                res.json({message: `Booking ${booking._id} has been confirmed by ${req.user.customData.user_id}`});
-            }
-        });
+         updateBookingStatusAndSendNotificaitonToStudent(req.body.booking_id, BookingStatus.accepted.name, res);
      }
      else{
         res.json({tutor_user_id: 0});
@@ -140,26 +207,29 @@ router.get('/', (req, res) => {
             
             var obj = JSON.parse(JSON.stringify(booking));
 
-            booking.forEach( function(b, index, theArray) {
-                User.findById(mongoose.Types.ObjectId(b.student_user_id), (err2, stu) => {
-                    if (err2)
-                        res.status(404).send(err2);
-                    User.findById(mongoose.Types.ObjectId(b.tutor_user_id), (err3, tut) => {
-                        if (err3)
-                            res.status(404).send(err3);
+            // this code block is async and does not execute before the response 
+
+            // booking.forEach( function(b, index, theArray) {
+            //     User.findById(mongoose.Types.ObjectId(b.student_user_id), (err2, stu) => {
+            //         if (err2)
+            //             res.status(404).send(err2);
+            //         User.findById(mongoose.Types.ObjectId(b.tutor_user_id), (err3, tut) => {
+            //             if (err3)
+            //                 res.status(404).send(err3);
                         
-                        if (stu)
-                            theArray[index]["student_name"] = stu.name;
-                        else
-                            theArray[index]["student_name"] = "N/A";
+            //             if (stu)
+            //                 theArray[index]["student_name"] = stu.name;
+            //             else
+            //                 theArray[index]["student_name"] = "N/A";
                         
-                        if (tut)
-                            theArray[index]["tutor_name"] = tut.name;
-                        else
-                            theArray[index]["tutor_name"] = "N/A";
-                    });
-                });
-            });
+            //             if (tut)
+            //                 theArray[index]["tutor_name"] = tut.name;
+            //             else
+            //                 theArray[index]["tutor_name"] = "N/A";
+            //         });
+            //     });
+            // });
+            
             res.json(booking);
         });
     }
