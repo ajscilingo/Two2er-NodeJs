fd2881ed82fdda5f0d1dc3a1f6fa40554fabc3af
Nithun Harikrishnan <nithun@gmail.com>
2017-05-07 13:57:23 -0500

    Location Schema updated with modifiedAt
    modifiedAt time stamp added, to denote location changes

--- models/schemas/location.js
+++ models/schemas/location.js
@@ -6,10 +6,11 @@ const Schema = mongoose.Schema;
 const LocationSchema = new Schema({
     type: {type: String, default: "Point"},
     coordinates: {type: [Number], default: [0,0]},
+    modifiedAt: {type: Date, default: Date.now},
     _id : {id: false}
 });
 
 // we need to export the schema here instead of the model.
-// the schema will be used in our User, StudentLocation and TutorLocation 
+// the schema will be used in our User, StudentLocation and TutorLocation
 // schemas to create documents with a location document embedded in each.
 module.exports = LocationSchema;
