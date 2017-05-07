5b8658af58bc4c41752f85ee2a0270680a21401c
Anthony Scilingo <ajscilingo@gmail.com>
2017-05-07 17:37:50 -0500

    Fixing endingDate
    needed to create a new instance of Date with scheduledmeetingdate passed in as a parameter to the constructor in order for it to clone the date.

--- routes/booking.js
+++ routes/booking.js
@@ -158,9 +158,7 @@ router.post('/request', (req, res) => {
     var booking = new Booking();
 
     var creationDate = new Date();
-
-    // make default ending date 1 hour from now
-    var endingDate = new Date();
+    var endingDate;
 
     var scheduledmeetingdate = (req.body.scheduledmeetingdate
       ? new Date(Date.parse(req.body.scheduledmeetingdate))
@@ -173,7 +171,8 @@ router.post('/request', (req, res) => {
     }
     
     // set the endingDate to same day as scheduledmeetingdate.
-    endingDate = scheduledmeetingdate;
+    // have to use the new Date constructor to make a copy of the scheduledmeetingdate
+    endingDate = new Date(scheduledmeetingdate);
     endingDate.setHours(scheduledmeetingdate.getHours() + 1);
 
     var scheduledendingdate = (req.body.scheduledendingdate
